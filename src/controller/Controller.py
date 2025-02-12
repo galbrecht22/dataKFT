960bbffeaaf276bdbcf782cccc2fd1a97f8d02df
gabor.albrecht <gabor611@gmail.com>
2025-02-12 21:10:16 +0100

    Added docstrings to Controller

--- src/controller/Controller.py
+++ src/controller/Controller.py
@@ -17,11 +17,27 @@ class Controller:
         self.mysql_controller = MySQLController()
 
     def init_environment(self, ingest=False):
+        """
+        Cleans up and initializes data objects for MongoDB and MySQL databases.
+
+        :parameter:
+         ingest: Boolean - whether to initialize MongoDB collection(s) for extract.
+        """
         if ingest:
             self.mongo_controller.build()
         self.mysql_controller.build()
 
     def extract_tender_list(self, date_filter: str = None):
+        """
+        Executes E2E process of fetching Tender objects from the respective endpoint and store them in MongoDB.
+
+        :parameter:
+         date_filter: str - date filter for retrieved list of Tender objects.
+                   Applied on object.awarded.{item}.date column.
+
+        :return:
+        Count of inserted documents.
+        """
         params = tenderList.ApiParameters()
         response = self.tender_list_api_controller.get_endpoint(params)
         results = self.tender_list_api_controller.get_all_paginated_results(response.page_count, params)
@@ -46,6 +62,14 @@ class Controller:
         return self.mongo_controller.store('tender', tenders)
 
     def extract_purchasers(self):
+        """
+        Executes E2E process of extracting Purchaser objects from TenderDetails retrieved from the respective endpoint
+        and store them in MongoDB.
+
+        :return:
+        Count of inserted documents.
+        """
+
         tenders = self.mongo_controller.load('tender')
         tender_details = [self.tender_details_api_controller.get_endpoint(tender['id']) for tender in tenders]
         print(f'Total records fetched: {len(tender_details)}')
@@ -59,6 +83,13 @@ class Controller:
         return self.mongo_controller.store('purchaser', purchasers)
 
     def extract_ships(self):
+        """
+        Executes E2E process of fetching Ships retrieved from the respective endpoint
+        and store them in MongoDB.
+
+        :return:
+        Count of inserted documents.
+        """
         self.ship_list_api_controller.authorize()
         results = self.ship_list_api_controller.get_endpoint('ships')
         ships = [item.__dict__ for item in results]
@@ -70,6 +101,13 @@ class Controller:
         return self.mongo_controller.store('ship', ships)
 
     def load_tenders(self):
+        """
+        Loads Tender objects from MongoDB, normalizes json structure to fit into relational dataset,
+        keeps necessary columns and loads them into MySQL.
+
+        :return:
+        Count of inserted rows.
+        """
         tenders = self.mongo_controller.load('tender')
 
         df = pd.json_normalize(tenders, max_level=1)
@@ -93,6 +131,13 @@ class Controller:
         return self.mysql_controller.bulk_insert('raw_tender', val)
 
     def load_purchasers(self):
+        """
+        Loads Purchaser objects from MongoDB, normalizes json structure to fit into relational dataset,
+        keeps necessary columns and loads them into MySQL.
+
+        :return:
+        Count of inserted rows.
+        """
         purchasers = self.mongo_controller.load('purchaser')
 
         df = pd.json_normalize(purchasers, max_level=1)
@@ -115,6 +160,12 @@ class Controller:
         return self.mysql_controller.bulk_insert('raw_purchaser', val)
 
     def load_ships(self):
+        """
+        Loads Ship objects from MongoDB and loads them into MySQL.
+
+        :return:
+        Count of inserted rows.
+        """
         ships = self.mongo_controller.load('ship')
 
         df = pd.DataFrame(ships)
@@ -124,5 +175,8 @@ class Controller:
         return self.mysql_controller.bulk_insert('raw_ship', val)
 
     def transform(self):
+        """
+        Executes predefined transformation logic on data objects.
+        """
         self.mysql_controller.populate(['stg_tender', 'stg_purchaser', 'stg_ship'])
         self.mysql_controller.populate(['t01_tender', 't02_purchaser', 't03_ship'])
